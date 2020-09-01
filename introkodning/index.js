57ae0068d419972b6abb1d90877e3aac98f0220b
gustavandreasson1 <70261430+gustavandreasson1@users.noreply.github.com>
2020-09-01 09:40:06 +0200

    han inte börja

---
+++ introkodning/index.js
@@ -0,0 +1,9 @@
+let text = "Hello"
+
+console.log(text);
+
+
+//if-sats - att göra val
+if (age) >= 18){
+    console.log("Du är vuxen")
+} else {
--- script/index.js
+++
@@ -1,3 +0,0 @@
-let text = "Hello"
-
-console.log(text);
