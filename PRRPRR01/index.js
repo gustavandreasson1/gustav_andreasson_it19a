c2680405c94e5ac01c524c8f506faadb2d295ed3
gustavandreasson1 <70261430+gustavandreasson1@users.noreply.github.com>
2020-09-03 09:51:57 +0200

    a

--- PRRPRR01/index.js
+++ PRRPRR01/index.js
@@ -44,6 +44,6 @@ function math(event){
     
     add.innerHTML = `${num1}+${num2}=${num1+num2}`
     sub.innerHTML = `${num1}-${num2}=${num1-num2}`
-    mult.innerHTML = `${num1}x${num2}=${num1xnum2}`
+    mult.innerHTML = `${num1}*${num2}=${num1*num2}`
 
 }
