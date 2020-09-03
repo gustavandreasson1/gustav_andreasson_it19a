0102b0073fa3d346ec401b3ec017e011da24f944
gustavandreasson1 <70261430+gustavandreasson1@users.noreply.github.com>
2020-09-03 09:00:57 +0200

    let

--- PRRPRR01/index.js
+++ PRRPRR01/index.js
@@ -1,6 +1,11 @@
-let text = "Hello"
 
-console.log(text);
+<head></head>
+
+
+let name = Gustav
+let age = 17
+
+console.log(`Hej ${name} du är ${age} år gammal`)
 
 
 //if-sats - att göra val
@@ -8,4 +13,6 @@ if (age) >= 18){
     console.log("Du är vuxen")
 } else {
     console.log("DU är barn")
-    
+}
+
+
