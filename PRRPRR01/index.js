05cb621a7e57bb5f6fadf49c4f78fb7c812798a4
gustavandreasson1 <70261430+gustavandreasson1@users.noreply.github.com>
2020-09-03 09:15:08 +0200

    matte

--- PRRPRR01/index.js
+++ PRRPRR01/index.js
@@ -1,11 +1,11 @@
 console.log("hej")
 
-let name = Gustav
-let age = 17
+let name = "Gustav"
+let age = "17"
 
 
 //if-sats - att göra val
-if (age) =18){
+if (age >=18){
     console.log("Du är vuxen")
 } else {
     console.log("Du är barn")
@@ -17,3 +17,15 @@ console.log(`Hej ${name} du är ${age} år gammal`)
 
 let text = document.querySelector("#text")
 text.innerHTML = (`${name} är ${age} år gammal`)
+
+
+let mult = document.querySelector("#multiplikation")
+let div = document.querySelector("#division")
+let add = document.querySelector("#addition")
+let sub = document.querySelector("#subtraktion")
+
+
+mult.innerHTML = `multiplikation`
+div.innerHTML = `division`
+add.innerHTML = `addition`
+sub.innerHTML = `subtraktion`
