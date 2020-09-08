54a1225dca58dda3ab3b9e2a39ebf4afa9c3dc73
gustavandreasson1 <70261430+gustavandreasson1@users.noreply.github.com>
2020-09-08 14:13:34 +0200

    nn

--- Variabler_IO/index.js
+++ Variabler_IO/index.js
@@ -6,3 +6,17 @@ name = "Gustav"
 //skriv ut "Hej namn"
 console.log(`Hej ${name}`)
 
+let tal1 = 3
+let tal2 = 4
+let tal3 = 5
+
+// 1. skapa variablerna tal1, tal2, tal3
+// 2. tilldela dessa variabler med värderna 3,4,5
+// 3 .räkna ut medelvärdet av dem
+
+medelvarde = (tal1+tal2+tal3)/3
+
+console.log(`(${tal1}+${tal2}+${tal3})/3 = ${medelvarde}`)
+
+
+let langd = prompt("ange värdet för rektangelns längd ")
